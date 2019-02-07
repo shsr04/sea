edc5512cfd12f4eb0de5aba8e906862f414335a3
Simon Heuser <simon.heuser@mni.thm.de>
2019-02-07 17:05:08 +0100

    fix BFS tests

--- test/bfs_test.cpp
+++ test/bfs_test.cpp
@@ -8,10 +8,6 @@
 
 using namespace Sealib;  // NOLINT
 
-static uint c1 = 0, c2 = 0;
-static void incr1(uint) { c1++; }
-static void incr2(uint, uint) { c2++; }
-
 static uint GRAPHCOUNT = 4, order = 500, degree = 20;
 static std::vector<DirectedGraph> makeGraphs() {
     std::vector<DirectedGraph> g;
@@ -21,17 +17,15 @@ static std::vector<DirectedGraph> makeGraphs() {
     return g;
 }
 
-class BFSTest : public ::testing::TestWithParam<DirectedGraph> {
- protected:
-    virtual void SetUp() { c1 = c2 = 0; }
-};
+class BFSTest : public ::testing::TestWithParam<DirectedGraph> {};
 
 INSTANTIATE_TEST_CASE_P(ParamTests, BFSTest, ::testing::ValuesIn(makeGraphs()),
                         /**/);
 
 TEST_P(BFSTest, userproc) {
+    uint c1 = 0, c2 = 0;
     DirectedGraph g = GetParam();
-    BFS bfs(&g, incr1, incr2);
+    BFS bfs(&g, [&](uint) { c1++; }, [&](uint, uint) { c2++; });
     bfs.init();
     bfs.forEach([](std::pair<uint, uint>) {});
     EXPECT_EQ(c1, order);
@@ -39,8 +33,9 @@ TEST_P(BFSTest, userproc) {
 }
 
 TEST_P(BFSTest, stdUserproc) {
+    uint c1 = 0, c2 = 0;
     DirectedGraph g = GetParam();
-    SimpleBFS bfs(&g, incr1, incr2);
+    SimpleBFS bfs(&g, [&](uint) { c1++; }, [&](uint, uint) { c2++; });
     bfs.init();
     bfs.forEach([](std::pair<uint, uint>) {});
     EXPECT_EQ(c1, order);
@@ -48,23 +43,25 @@ TEST_P(BFSTest, stdUserproc) {
 }
 
 TEST(BFSTest, nextComponent) {
+    uint c1 = 0, c2 = 0;
     DirectedGraph g = GraphCreator::kOutdegree(order, 0);
-    BFS bfs(&g, incr1, incr2);
-    uint rc = 0;
+    BFS bfs(&g, [&](uint) { c1++; }, [&](uint, uint) { c2++; });
+    uint cc = 0;
     bfs.init();
-    bfs.forEach([&](std::pair<uint, uint>) { rc++; });
-    EXPECT_EQ(rc, order);
+    bfs.forEach([&](std::pair<uint, uint>) { cc++; });
+    EXPECT_EQ(cc, order);
     EXPECT_EQ(c1, order);
     EXPECT_EQ(c2, 0);
 }
 
 TEST(SimpleBFSTest, nextComponent) {
+    uint c1 = 0, c2 = 0;
     DirectedGraph g = GraphCreator::kOutdegree(order, 0);
-    SimpleBFS bfs(&g, incr1, incr2);
-    uint rc = 0;
+    SimpleBFS bfs(&g, [&](uint) { c1++; }, [&](uint, uint) { c2++; });
+    uint cc = 0;
     bfs.init();
-    bfs.forEach([&](std::pair<uint, uint>) { rc++; });
-    EXPECT_EQ(rc, order);
+    bfs.forEach([&](std::pair<uint, uint>) { cc++; });
+    EXPECT_EQ(cc, order);
     EXPECT_EQ(c1, order);
     EXPECT_EQ(c2, 0);
 }
